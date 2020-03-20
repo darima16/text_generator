ebb87dfff405ce8381bf04047fa7e8e93dc10216
darima16 <44423474+darima16@users.noreply.github.com>
2020-03-21 00:34:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,3 +39,17 @@ def creat_dict(lst_words):
         words = []
     d.update({lst_words[len(lst_words) - 1]: []})
     return d
+
+def stop_words(dct_words):
+    stop = []
+    words = dct_words.keys()
+    for word in words:
+        if word[-1] == '.' or word[-1] == '!' or word[-1] == '?':
+            stop.append(word)
+    return stop
+
+def main():
+    file = open_file()
+    lst_words = text_editing(file_read(file))
+    dct_words = creat_dict(lst_words)
+    sent = int(input('Number of sentences: '))
